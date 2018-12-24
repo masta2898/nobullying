688a8e6c9b80a469cc596ef85711ef1e04a2cac7
Master <kulikov.paul99@gmail.com>
2018-12-24 18:06:26 +0200

    realized beginModeratingReport

--- src/main/java/ua/sumy/stpp/nobullying/service/ReportService.java
+++ src/main/java/ua/sumy/stpp/nobullying/service/ReportService.java
@@ -7,6 +7,7 @@ import ua.sumy.stpp.nobullying.model.Report;
 import ua.sumy.stpp.nobullying.service.error.BadReportException;
 import ua.sumy.stpp.nobullying.service.error.ReportIsAlreadyFinishedException;
 import ua.sumy.stpp.nobullying.service.error.ReportIsAlreadyModeratingException;
+import ua.sumy.stpp.nobullying.service.error.ReportNotFoundException;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -47,12 +48,32 @@ public class ReportService implements Service {
         return (reports != null) ? reports : new LinkedList<>();
     }
 
-    void beginModeratingReport(long id) throws ReportIsAlreadyModeratingException {
+    void beginModeratingReport(long id) throws BadReportException, ReportNotFoundException,
+            ReportIsAlreadyModeratingException, ReportIsAlreadyFinishedException {
+        if (getReportById(id).isNull()) {
+            log.severe(String.format("Error beginning moderating report: no report found by id %d", id));
+            throw new ReportNotFoundException("Report doesn't exist!");
+        }
 
+        Report report = (Report) getReportById(id);
+        Report.ProcessingState state = report.getState();
+
+        if (state == Report.ProcessingState.MODERATING) {
+            log.warning(String.format("Attempt to moderate already moderating report (%d).", id));
+            throw new ReportIsAlreadyModeratingException("Report cannot be moderated twice!");
+        }
+
+        if (state == Report.ProcessingState.FINISHED) {
+            log.warning(String.format("Attempt to moderate already finished report (%d)", id));
+            throw new ReportIsAlreadyFinishedException("Finished report cannot be moderated!");
+        }
+
+        saveNewReportState(Report.ProcessingState.MODERATING, report);
     }
 
     void finishModeratingReport(long id) throws ReportIsAlreadyFinishedException {
 
+        saveNewReportState(Report.ProcessingState.FINISHED, report);
     }
 
     void saveReport(Report report) throws BadReportException {
@@ -61,5 +82,16 @@ public class ReportService implements Service {
 
     void deleteReport(long id) throws BadReportException {
 
+    }
+
+    private void saveNewReportState(Report.ProcessingState state, Report report) throws BadReportException {
+        report.setState(state);
+        try {
+            saveReport(report);
+            log.info(String.format("Began moderating report (%d)", report.getId()));
+        } catch (BadReportException e) {
+            log.severe(String.format("Error saving report new state (%s): %s", state, e));
+            throw e;
+        }
     }
 }
