c8a7d0b3e50f210811e24aaa52db1032039ba56f
Master <kulikov.paul99@gmail.com>
2018-12-25 22:00:56 +0200

    begin working on main servlet

--- src/main/java/ua/sumy/stpp/nobullying/controller/NoBullyingServlet.java
+++ src/main/java/ua/sumy/stpp/nobullying/controller/NoBullyingServlet.java
@@ -5,14 +5,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 public class NoBullyingServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        req.getRequestDispatcher("/jsp/nobullying.jsp").forward(req, resp);
-//        PrintWriter out = resp.getWriter();
-//        out.println("Hello, world!");
         getServletContext().getRequestDispatcher("/jsp/nobullying.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
     }
 }
